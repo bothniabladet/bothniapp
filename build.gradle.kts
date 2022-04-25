33bf09d882f7a7f19a63d4ee034ddcd9dd7430ac
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2022-04-25 15:00:08 +0200

    Initial database configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("org.jetbrains.exposed", "exposed-core", "0.38.1")
     implementation("org.jetbrains.exposed", "exposed-dao", "0.38.1")
     implementation("org.jetbrains.exposed", "exposed-jdbc", "0.38.1")
+    implementation("org.postgresql:postgresql:42.2.2")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
---
+++ src/main/kotlin/se/ltu/student/plugins/Database.kt
@@ -0,0 +1,14 @@
+package se.ltu.student.plugins
+
+import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.Database
+
+object DB {
+    val connection by lazy {
+        Database.connect()
+    }
+}
+
+fun Application.getDatabase(): Database {
+    return DB.connection
+}
