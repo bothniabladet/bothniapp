c1e5b295545f34267825ff77d723811cfdb21269
VikkeNy <modoviktor@hotmail.com>
2022-04-25 13:42:42 +0200

    Ett kreativt meddelande

---
+++ src/main/kotlin/se/ltu/student/Application.kt
@@ -0,0 +1,16 @@
+package se.ltu.student
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import se.ltu.student.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSecurity()
+        configureHTTP()
+        configureMonitoring()
+        configureTemplating()
+        configureSerialization()
+    }.start(wait = true)
+}
