4d28a7cb1366efd6006ae38fe678fe664fe21087
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2022-05-03 18:27:32 +0200

    Added initial upload endpoint

--- src/main/kotlin/se/ltu/student/modules/UploadModule.kt
+++ src/main/kotlin/se/ltu/student/modules/UploadModule.kt
@@ -1,15 +1,34 @@
 package se.ltu.student.modules
 
+import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureModuleUpload() {
     routing {
         route("/upload") {
             get {
                 call.respond(FreeMarkerContent("upload/index.ftl", null))
+            }
+
+            post {
+                val multipartData = call.receiveMultipart()
+                multipartData.forEachPart { part ->
+                    when (part) {
+                        is PartData.FileItem -> {
+                            val fileName = part.originalFileName as String
+                            val fileBytes = part.streamProvider().readBytes()
+                            File("uploads/$fileName").writeBytes(fileBytes)
+                        }
+                        else -> {}
+                    }
+                }
+                call.respond(HttpStatusCode.OK)
             }
         }
     }
--- src/main/resources/templates/upload/index.ftl
+++ src/main/resources/templates/upload/index.ftl
@@ -1,4 +1,15 @@
 <#import "../_layout.ftl" as layout />
 <@layout.header>
     <h1>Upload</h1>
+    <form class="g-5" action="/upload" method="post" enctype="multipart/form-data">
+        <div class="input-group mb-3 container">
+            <label for="formFileMultiple" class="form-label"></label>
+            <input class="form-control" type="file" id="formFileMultiple" name="images" multiple />
+        </div>
+        <div class="d-grid gap-4 container inputBox text-center">
+            <button type="submit" class="btn btn-primary">Ladda upp bilder</button>
+
+            <a class="btn btn-primary" href="main.html" role="button">Gå tillbaka</a>
+        </div>
+    </form>
 </@layout.header>
