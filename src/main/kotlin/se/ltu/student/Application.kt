855498ab7cc58ba8537f94d29694fcff6728a6e5
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2022-05-03 09:48:46 +0200

    Squashed commit of the following:
    commit 094fed63fefc03f83c9149dec6df12b94220d905
    Author: Maximilian Wendel <maximilian.wendel@mwtdev.se>
    Date:   Tue May 3 09:47:09 2022 +0200
    
        Added a README
    
    commit 751dcc5bcbb60284354254a254204a6c16f0921b
    Author: Maximilian Wendel <maximilian.wendel@mwtdev.se>
    Date:   Tue May 3 09:46:54 2022 +0200
    
        Added Dockerfile
    
    commit 94aaae084e69c4343d6660e97521a6591cad1204
    Author: Maximilian Wendel <maximilian.wendel@mwtdev.se>
    Date:   Tue May 3 09:46:31 2022 +0200
    
        Prepared application for running in a container

--- src/main/kotlin/se/ltu/student/Application.kt
+++ src/main/kotlin/se/ltu/student/Application.kt
@@ -1,26 +1,25 @@
 package se.ltu.student
 
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.application.*
 import se.ltu.student.modules.*
 import se.ltu.student.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", watchPaths = listOf("classes", "resources")) {
-        configureDatabase()
-        configureTemplating()
-        configureRouting()
-        configureSecurity()
-        configureHTTP()
-        configureMonitoring()
-        configureSerialization()
-        configureAuthentication()
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-        // Modules
-        configureModuleAuthentication()
-        configureModuleUser()
-        configureModuleArchive()
-        configureModuleSearch()
-        configureModuleUpload()
-    }.start(wait = true)
+fun Application.module() {
+    configureDatabase()
+    configureTemplating()
+    configureRouting()
+    configureSecurity()
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureAuthentication()
+
+    // Modules
+    configureModuleAuthentication()
+    configureModuleUser()
+    configureModuleArchive()
+    configureModuleSearch()
+    configureModuleUpload()
 }
